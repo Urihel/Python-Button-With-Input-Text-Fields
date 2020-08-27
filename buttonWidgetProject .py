1cbafc5ad6ac7005e801d7f570282696bb3a49b9
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-08-26 20:35:51 -0700

    Update buttonWidgetProject .py

--- buttonWidgetProject .py
+++ buttonWidgetProject .py
@@ -13,6 +13,13 @@ def myClick():
     myLabel = Label(root,text="Hello " + e.get())
     myLabel.pack()
 
+# how to call back to myClick function 
+
+#in the Button() function you can pass an argument called command 
+#with command you can assign it the value of you function name and 
+#when the button is clicked you can have a function execute some code.
+#in this example when you click the button a label is executed. inside 
+# the label a message in the form of text is also execute 
 myButton = Button(root, text="Enter Your Name", padx=50, pady=50, command=myClick,fg="green",bg="grey")
 myButton.pack()
 
