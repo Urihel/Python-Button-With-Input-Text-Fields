2fb738b7e48e1ecb7e28c0c8b75c5d205732532a
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-08-26 20:45:59 -0700

    Update buttonWidgetProject .py

--- buttonWidgetProject .py
+++ buttonWidgetProject .py
@@ -5,6 +5,14 @@ e = Entry(root,width=50,bg="skyblue",fg="white")
 e.pack()
 
 def myClick():
+    #Since python is super flexiable and an OOPs language you can 
+    #pass an argument inside of Label() called text which allows 
+    #to create some text inside that label. great thing about the text
+    #argument is that you can concatinate strings with built in functions
+    #like .get() which retrieves the text that you enter into our Entry() function 
+    #so our input text fields. 
+    
+    
     #you can also use variables inside your click button function
     # so x = "hello"
     # so y = e.get()
@@ -16,7 +24,7 @@ def myClick():
 # how to call back to myClick function 
 
 #in the Button() function you can pass an argument called command 
-#with command you can assign it the value of you function name and 
+#with command you can assign it the value of your function name and 
 #when the button is clicked you can have a function execute some code.
 #in this example when you click the button a label is executed. inside 
 # the label a message in the form of text is also execute 
